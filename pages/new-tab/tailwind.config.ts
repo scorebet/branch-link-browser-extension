05bb4bad7e2be3639821162663186f718c1f251a
Aayush Manocha <aayush.manocha@thescore.com>
2025-04-16 19:10:38 -0400

    fixed new tab page styling

--- pages/new-tab/tailwind.config.ts
+++ pages/new-tab/tailwind.config.ts
@@ -1,7 +1,14 @@
-import baseConfig from '@extension/tailwindcss-config';
-import { withUI } from '@extension/ui';
+import baseConfig from '@extension/tailwindcss-config'
+import { withUI } from '@extension/ui'
 
 export default withUI({
   ...baseConfig,
   content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
-});
+  theme: {
+    extend: {
+      colors: {
+        'brand-green-link': '#02644F',
+      },
+    },
+  },
+})
