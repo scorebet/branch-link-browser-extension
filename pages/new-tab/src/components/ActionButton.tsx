b8147a6933dfa79b30cb88752e4940f6a4030bbc
Aayush Manocha <aayush.manocha@thescore.com>
2025-05-08 17:57:08 -0400

    address comments

--- pages/new-tab/src/components/ActionButton.tsx
+++ pages/new-tab/src/components/ActionButton.tsx
@@ -1,10 +1,23 @@
 import SvgDelete from './SvgDelete'
 import SvgEdit from './SvgEdit'
 
+type Variant = 'edit' | 'delete'
+
 type ActionButtonProps = {
   onClick: () => void
   children: React.ReactNode
-  variant?: 'edit' | 'delete'
+  variant?: Variant
+}
+
+const ActionButtonIcon = ({ variant }: { variant: Variant }) => {
+  switch (variant) {
+    case 'edit':
+      return <SvgEdit />
+    case 'delete':
+      return <SvgDelete />
+    default:
+      return null
+  }
 }
 
 export default function ActionButton({ onClick, variant, children }: ActionButtonProps) {
@@ -12,7 +25,7 @@ export default function ActionButton({ onClick, variant, children }: ActionButto
     <button
       onClick={onClick}
       className="flex items-center gap-x-2 stroke-slate-500 hover:stroke-brand-green-link hover:bg-brand-background-green w-full py-2">
-      {variant === 'edit' ? <SvgEdit /> : <SvgDelete />}
+      <ActionButtonIcon variant={variant!} />
       {children}
     </button>
   )
